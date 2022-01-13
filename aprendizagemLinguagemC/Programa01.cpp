1e4e218c6d11328d12173d67f5f6f9a32bb3d998
Amarante <amarantegba@gmail.com>
2022-01-13 12:54:18 -0300

    atualizações 13_01_2022

---
+++ aprendizagemLinguagemC/Programa01.cpp
@@ -0,0 +1,11 @@
+// Programa de impressão de texto.
+#include <iostream> // permite que o programa gere saída de dados na tela
+// a função main inicia a execução do programa
+int main()
+  {
+	std::cout << "Primeiro Programa em C++!\n"; // exibe a mensagem
+	// Imprimindo uma linha de texto com múltiplas instruções.
+	std::cout << "Primeiro Programa ";
+	std::cout << "em linguagem C++!\n";
+	return 0; // indica que o programa terminou com sucesso
+  } 
---
+++ aprendizagemLinguagemC/Programa02.cpp
@@ -0,0 +1,9 @@
+// Imprimindo uma linha de texto com múltiplas instruções.
+#include <iostream> // permite que o programa gere saída de dados na tela
+// a função main inicia a execução do programa
+int main()
+  {
+	std::cout << "Primeiro Programa ";
+	std::cout << " em linguagem C++!\n";
+	return 0; // indica que o programa terminou com sucesso
+  } // fim da função main
